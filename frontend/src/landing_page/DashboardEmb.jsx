c4bb07d590433ca755407eaeaf61de9f7514f233
srinidhi2006 <160821690+srinidhi2006@users.noreply.github.com>
2025-06-13 13:04:53 +0530

    Update DashboardEmb.jsx

--- frontend/src/landing_page/DashboardEmb.jsx
+++ frontend/src/landing_page/DashboardEmb.jsx
@@ -2,7 +2,7 @@ const DashboardEmbed = () => {
   return (
     <div style={{ height: "100vh" }}>
       <iframe
-        src="http://localhost:3001"
+        src="https://zerodha-clone-dashboard-d6ln.onrender.com"
         title="Dashboard"
         width="100%"
         height="100%"
